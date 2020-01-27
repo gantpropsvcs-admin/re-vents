f76ba0f1e2e922f8357d813b3739f6b97c34da87
gantpropsvcs-admin <58055804+gantpropsvcs-admin@users.noreply.github.com>
2020-01-27 11:26:41 -0800

    Add form submit and two way state

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsFromDashboard = [
     {
@@ -64,6 +65,15 @@ class EventDashboard extends Component {
         this.setState(({ isOpen }) => ({ isOpen: !isOpen }));
     };
 
+    handleCreateEvent = newEvent => {
+        newEvent.id = cuid();
+        newEvent.hostPhotoURL = '/assets/user.png';
+        this.setState(({ events }) => ({
+            events: [...events, newEvent],
+            isOpen: false,
+        }));
+    };
+
     render() {
         const { events, isOpen } = this.state;
 
@@ -82,6 +92,7 @@ class EventDashboard extends Component {
                         />
                         {isOpen && (
                             <EventForm
+                                createEvent={this.handleCreateEvent}
                                 cancelFormOpen={this.handleIsOpenToggle}
                             />
                         )}
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,32 +2,76 @@ import React, { Component } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
 class EventForm extends Component {
+    state = {
+        title: '',
+        date: '',
+        city: '',
+        venue: '',
+        hostedBy: '',
+    };
+
+    handleFormSubmit = evt => {
+        this.props.createEvent(this.state);
+        evt.preventDefault();
+    };
+
+    handleInputChange = ({ target: { name, value } }) => {
+        this.setState({ [name]: value });
+    };
+
     render() {
         const { cancelFormOpen } = this.props;
+        const { title, date, city, venue, hostedBy } = this.state;
 
         return (
             <div>
                 <Segment>
-                    <Form>
+                    <Form onSubmit={this.handleFormSubmit} autoComplete="off">
                         <Form.Field>
                             <label>Event Title</label>
-                            <input placeholder="First Name" />
+                            <input
+                                name="title"
+                                onChange={this.handleInputChange}
+                                placeholder="Event Title"
+                                value={title}
+                            />
                         </Form.Field>
                         <Form.Field>
                             <label>Event Date</label>
-                            <input type="date" placeholder="Event Date" />
+                            <input
+                                type="date"
+                                placeholder="Event Date"
+                                name="date"
+                                onChange={this.handleInputChange}
+                                value={date}
+                            />
                         </Form.Field>
                         <Form.Field>
                             <label>City</label>
-                            <input placeholder="City event is taking place" />
+                            <input
+                                placeholder="City event is taking place"
+                                name="city"
+                                onChange={this.handleInputChange}
+                                value={city}
+                            />
                         </Form.Field>
                         <Form.Field>
                             <label>Venue</label>
-                            <input placeholder="Enter the Venue of the event" />
+                            <input
+                                placeholder="Enter the Venue of the event"
+                                name="venue"
+                                onChange={this.handleInputChange}
+                                value={venue}
+                            />
                         </Form.Field>
                         <Form.Field>
                             <label>Hosted By</label>
-                            <input placeholder="Enter the name of person hosting" />
+                            <input
+                                placeholder="Enter the name of person hosting"
+                                name="hostedBy"
+                                onChange={this.handleInputChange}
+                                value={hostedBy}
+                            />
                         </Form.Field>
                         <Button positive type="submit">
                             Submit
